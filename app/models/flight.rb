049c412987bfe8dad308992e2e800b717ba380e1
Unitato1 <robertsvihla@gmail.com.github.com>
2024-03-25 21:28:28 +0100

    Done

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,7 @@
 class Flight < ApplicationRecord
+  def find_matching(departure, arival, departure_date, arival_date)
+    where(departure_airport_id: departure)
+  end
   belongs_to :arrival_airport, class_name: "Airport", foreign_key: "arrival_airport_id"
   belongs_to :departure_airport, class_name: "Airport", foreign_key: "departure_airport_id"
 end
